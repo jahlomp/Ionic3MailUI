26228dacf23217a1b8c84de260252d9ad308607c
Precious Jahlom Agboado <jahlom@preterk.com>
2017-12-16 05:28:56 +0000

    Some small additions and bug fixes

--- src/pages/inbox/inbox.ts
+++ src/pages/inbox/inbox.ts
@@ -14,7 +14,78 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'inbox.html',
 })
 export class InboxPage {
-
+  messages = [
+    {
+      name: "Adam Bradley",
+      image: "adam-bradley.jpg",
+      subject:"This town ain't big enough for the two of us!",
+      message: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque error, ut distinctio maxime non hic adipisci suscipit inventore dignissimos ad, harum, vero ullam odio ab nulla culpa enim sit rem!",
+      date_recieved: new Date("2017-05-09")
+    },
+    {
+      name: "Ben Sperry",
+      image: "ben-sperry.jpg",
+      subject:"This town ain't big enough for the two of us!",
+      message: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque error, ut distinctio maxime non hic adipisci suscipit inventore dignissimos ad, harum, vero ullam odio ab nulla culpa enim sit rem!",
+      date_recieved: new Date("2017-06-10")
+    },
+    {
+      name: "Justin Willis",
+      image: "justin-willis.jpg",
+      subject:"This town ain't big enough for the two of us!",
+      message: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque error, ut distinctio maxime non hic adipisci suscipit inventore dignissimos ad, harum, vero ullam odio ab nulla culpa enim sit rem!",
+      date_recieved: new Date("2017-07-03")
+    },
+    {
+      name: "Adam Bradley",
+      image: "adam-bradley.jpg",
+      subject:"This town ain't big enough for the two of us!",
+      message: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque error, ut distinctio maxime non hic adipisci suscipit inventore dignissimos ad, harum, vero ullam odio ab nulla culpa enim sit rem!",
+      date_recieved: new Date()
+    },
+    {
+      name: "Adam Bradley",
+      image: "adam-bradley.jpg",
+      subject:"This town ain't big enough for the two of us!",
+      message: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque error, ut distinctio maxime non hic adipisci suscipit inventore dignissimos ad, harum, vero ullam odio ab nulla culpa enim sit rem!",
+      date_recieved: new Date("2017-05-09")
+    },
+    {
+      name: "Adam Bradley",
+      image: "adam-bradley.jpg",
+      subject:"This town ain't big enough for the two of us!",
+      message: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque error, ut distinctio maxime non hic adipisci suscipit inventore dignissimos ad, harum, vero ullam odio ab nulla culpa enim sit rem!",
+      date_recieved: new Date("2017-05-09")
+    },
+    {
+      name: "Adam Bradley",
+      image: "adam-bradley.jpg",
+      subject:"This town ain't big enough for the two of us!",
+      message: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque error, ut distinctio maxime non hic adipisci suscipit inventore dignissimos ad, harum, vero ullam odio ab nulla culpa enim sit rem!",
+      date_recieved: new Date("2017-05-09")
+    },
+    {
+      name: "Adam Bradley",
+      image: "adam-bradley.jpg",
+      subject:"This town ain't big enough for the two of us!",
+      message: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque error, ut distinctio maxime non hic adipisci suscipit inventore dignissimos ad, harum, vero ullam odio ab nulla culpa enim sit rem!",
+      date_recieved: new Date("2017-05-09")
+    },
+    {
+      name: "Adam Bradley",
+      image: "adam-bradley.jpg",
+      subject:"This town ain't big enough for the two of us!",
+      message: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque error, ut distinctio maxime non hic adipisci suscipit inventore dignissimos ad, harum, vero ullam odio ab nulla culpa enim sit rem!",
+      date_recieved: new Date("2017-05-09")
+    },
+    {
+      name: "Adam Bradley",
+      image: "adam-bradley.jpg",
+      subject:"This town ain't big enough for the two of us!",
+      message: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque error, ut distinctio maxime non hic adipisci suscipit inventore dignissimos ad, harum, vero ullam odio ab nulla culpa enim sit rem!",
+      date_recieved: new Date("2017-05-09")
+    }
+  ]
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
@@ -22,4 +93,7 @@ export class InboxPage {
     console.log('ionViewDidLoad InboxPage');
   }
 
+  readMessage(message) {
+    this.navCtrl.push("MessagePage", { message: message })
+  }
 }
